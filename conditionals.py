d2a7e8fbf90ca2e96ae512fb7abed221f1f80396
estherAdej <estheradejayan1@gmail.com>
2022-10-20 14:55:02 +0100

    loops

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+#Write a program which asks for an input of a mark, then checks to see if the mark is greater than 65. 
+#If so, print out Pass. If not, print Fail
+
+mark = int(input("what is your mark:"))
+
+if mark > 65:
+    print("pass")
+else:
+    print ("fail")
+
